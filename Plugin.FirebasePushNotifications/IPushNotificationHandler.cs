cfc12a7857a62091cfee2bb39c2b663c020ebe9c
Thomas Galliker <thomas@bluewin.ch>
2023-09-28 06:54:24 +0200

    Add comments

--- Plugin.FirebasePushNotifications/IPushNotificationHandler.cs
+++ Plugin.FirebasePushNotifications/IPushNotificationHandler.cs
@@ -1,16 +1,23 @@
-﻿
-
-using System.Collections.Generic;
-
-namespace Plugin.FirebasePushNotifications
+﻿namespace Plugin.FirebasePushNotifications
 {
     public interface IPushNotificationHandler
     {
-        //Method triggered when an error occurs
+        /// <summary>
+        /// Method triggered when an error occurs.
+        /// </summary>
+        /// <param name="error">The error message.</param>
         void OnError(string error);
-        //Method triggered when a notification is opened
+
+        /// <summary>
+        /// Method triggered when a notification is opened
+        /// </summary>
+        /// <param name="response">The notification response.</param>
         void OnOpened(NotificationResponse response);
-        //Method triggered when a notification is received
+
+        /// <summary>
+        /// Method triggered when a notification is received.
+        /// </summary>
+        /// <param name="parameters">The notification data.</param>
         void OnReceived(IDictionary<string, object> parameters);
     }
 }
