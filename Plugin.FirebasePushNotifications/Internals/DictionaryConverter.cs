6927ee2d1a33ac3521e0684929e15e5edc1f49d8
Thomas Galliker <thomas@bluewin.ch>
2023-10-05 23:53:53 +0200

    Add flat dictionary to object converter + tests

---
+++ Plugin.FirebasePushNotifications/Internals/DictionaryConverter.cs
@@ -0,0 +1,193 @@
+﻿using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Plugin.FirebasePushNotifications.Internals
+{
+    /// <summary>
+    /// Converts any object into a flat dictionary with string key and string value.
+    /// All nested properties are flattened into a dot-separated structure.
+    /// </summary>
+    public class DictionaryConverter
+    {
+        /// <summary>
+        /// Creates a flat dictionary for <paramref name="source"/> of type <typeparamref name="T"/>.
+        /// </summary>
+        /// <param name="source">Source object.</param>
+        /// <returns>Flat dictionary with path as key and value as string.</returns>
+        public static IDictionary<string, string> Flatten<T>(T source)
+        {
+            var jObject = JObject.FromObject(source);
+            return Flatten(jObject);
+        }
+
+        /// <summary>
+        /// Creates a flat dictionary for <paramref name="source"/> of type <typeparamref name="T"/>.
+        /// </summary>
+        /// <param name="source">Source object.</param>
+        /// <param name="jsonSerializer">Custom JsonSerializer used to serialize <paramref name="source"/>.</param>
+        /// <returns>Flat dictionary with path as key and value as string.</returns>
+        public static IDictionary<string, string> Flatten<T>(T source, JsonSerializer jsonSerializer)
+        {
+            var jObject = JObject.FromObject(source, jsonSerializer);
+            return Flatten(jObject);
+        }
+
+        /// <summary>
+        /// Creates a flat dictionary for <paramref name="source"/> of type <seealso cref="JObject"/>.
+        /// </summary>
+        /// <param name="source">Source object.</param>
+        /// <returns>Flat dictionary with path as key and value as string.</returns>
+        public static IDictionary<string, string> Flatten(JObject source)
+        {
+            var jTokens = source
+                .Descendants()
+                .Where(p => !p.Any());
+
+            var results = jTokens
+                .Select(jToken =>
+                {
+                    var value = (jToken as JValue)?.Value?.ToString();
+                    var key = jToken.Path;
+                    return new KeyValuePair<string, string>(key, value);
+                })
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            return results;
+        }
+
+        /// <summary>
+        /// Unflattens a source <paramref name="dictionary"/> into an target object of type <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">Target object type.</typeparam>
+        /// <param name="dictionary">Source dictionary.</param>
+        /// <returns>Target object.</returns>
+        public static T Unflatten<T>(IDictionary<string, string> dictionary)
+        {
+            var jObject = Unflatten(dictionary);
+            return jObject.ToObject<T>();
+        }
+
+        /// <inheritdoc cref="Unflatten{T}(IDictionary{string, string})"/>
+        /// <param name="jsonSerializer">A custom json serializer.</param>
+        public static T Unflatten<T>(IDictionary<string, string> dictionary, JsonSerializer jsonSerializer)
+        {
+            var jObject = Unflatten(dictionary);
+            return jObject.ToObject<T>(jsonSerializer);
+        }
+
+        public static object Unflatten(IDictionary<string, string> dictionary, Type targetType, JsonSerializer jsonSerializer)
+        {
+            var jObject = Unflatten(dictionary);
+            return jObject.ToObject(targetType, jsonSerializer);
+        }
+
+        /// <summary>
+        ///  Creates a JObject by a given flat dictionary.
+        /// </summary>
+        /// <param name="dictionary">Flat dictionary.</param>
+        /// <returns> Hierarchical JObject.</returns>
+        public static JObject Unflatten(IDictionary<string, string> dictionary)
+        {
+            JContainer result = null;
+            var setting = new JsonMergeSettings { MergeArrayHandling = MergeArrayHandling.Merge };
+            foreach (var pathValue in dictionary)
+            {
+                if (result == null)
+                {
+                    result = UnflattenSingle(pathValue);
+                }
+                else
+                {
+                    result.Merge(UnflattenSingle(pathValue), setting);
+                }
+            }
+
+            return result as JObject;
+        }
+
+        private static JContainer UnflattenSingle(KeyValuePair<string, string> keyValue)
+        {
+            var path = keyValue.Key;
+            var value = keyValue.Value;
+            var pathSegments = SplitPath(path);
+
+            JContainer lastItem = null;
+            
+            // Build from leaf to root
+            foreach (var pathSegment in pathSegments.Reverse())
+            {
+                var type = GetJsonTokenType(pathSegment);
+                switch (type)
+                {
+                    case JTokenType.Object:
+                        var obj = new JObject();
+                        if (null == lastItem)
+                        {
+                            obj.Add(pathSegment, value);
+                        }
+                        else
+                        {
+                            obj.Add(pathSegment, lastItem);
+                        }
+
+                        lastItem = obj;
+                        break;
+                    case JTokenType.Array:
+                        var array = new JArray();
+                        var index = GetArrayIndex(pathSegment);
+                        array = FillEmpty(array, index);
+                        if (lastItem == null)
+                        {
+                            array[index] = value;
+                        }
+                        else
+                        {
+                            array[index] = lastItem;
+                        }
+
+                        lastItem = array;
+                        break;
+                    default:
+                        throw new NotSupportedException($"UnflattenSingle does not support type {type}.");
+                }
+            }
+
+            return lastItem;
+        }
+
+        public static IEnumerable<string> SplitPath(string path)
+        {
+            var reg = new Regex(@"(?!\.)([^. ^\[\]]+)|(?!\[)(\d+)(?=\])");
+            foreach (Match match in reg.Matches(path))
+            {
+                yield return match.Value;
+            }
+        }
+
+        private static JArray FillEmpty(JArray array, int index)
+        {
+            for (var i = 0; i <= index; i++)
+            {
+                array.Add(null);
+            }
+
+            return array;
+        }
+
+        private static JTokenType GetJsonTokenType(string pathSegment)
+        {
+            return int.TryParse(pathSegment, out _) ? JTokenType.Array : JTokenType.Object;
+        }
+
+        private static int GetArrayIndex(string pathSegment)
+        {
+            if (int.TryParse(pathSegment, out var result))
+            {
+                return result;
+            }
+
+            throw new Exception("Unable to parse array index: " + pathSegment);
+        }
+    }
+}
--- Tests/Plugin.FirebasePushNotifications.Tests/Internals/DictionaryConverterTests.cs
+++ Tests/Plugin.FirebasePushNotifications.Tests/Internals/DictionaryConverterTests.cs
@@ -1,17 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FluentAssertions;
+﻿using FluentAssertions;
 using Plugin.FirebasePushNotifications.Internals;
-using Plugin.FirebasePushNotifications.Model;
-using Xunit;
+using Xunit.Abstractions;
 
 namespace Plugin.FirebasePushNotifications.Tests.Internals
 {
-    public class DictionaryJsonConverterTests
+    public class DictionaryConverterTests
     {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public DictionaryConverterTests(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
         [Fact]
         public void ShouldFlattenObjectToDictionary()
         {
@@ -19,24 +20,29 @@ namespace Plugin.FirebasePushNotifications.Tests.Internals
             var source = new TestObject
             {
                 Title = "Title",
-                Body = "Body",
+                Date = new DateTime(2000, 1, 1, 12, 00, 00, DateTimeKind.Utc),
                 NestedTestObject = new NestedTestObject
                 {
                     Title = "NestedTitle",
-                }
+                },
+                TestArray = new[] { "value1", "value2" }
             };
 
             // Act
-            var json = DictionaryJsonConverter.Flatten(source);
+            var json = DictionaryConverter.Flatten(source);
 
             // Assert
+            this.testOutputHelper.WriteLine(ObjectDumper.Dump(json, DumpStyle.CSharp));
+
             json.Should().NotBeNull();
             json.Should().BeEquivalentTo(
                 new Dictionary<string, string>
                 {
                     { "Title", "Title" },
-                    { "Body", "Body" },
-                    { "NestedTestObject.Title", "NestedTitle" }
+                    { "Date", "01.01.2000 12:00:00" },
+                    { "NestedTestObject.Title", "NestedTitle" },
+                    { "TestArray[0]", "value1" },
+                    { "TestArray[1]", "value2" }
                 });
         }
 
@@ -47,24 +53,29 @@ namespace Plugin.FirebasePushNotifications.Tests.Internals
             var source = new Dictionary<string, string>
             {
                 { "Title", "Title" },
-                { "Body", "Body" },
-                { "NestedTestObject.Title", "NestedTitle" }
+                { "Date", "01.01.2000 12:00:00" },
+                { "NestedTestObject.Title", "NestedTitle" },
+                { "TestArray[0]", "value1" },
+                { "TestArray[1]", "value2" }
             };
 
             // Act
-            var target = DictionaryJsonConverter.Unflatten<TestObject>(source);
+            var target = DictionaryConverter.Unflatten<TestObject>(source);
 
             // Assert
+            this.testOutputHelper.WriteLine(ObjectDumper.Dump(target, DumpStyle.CSharp));
+
             target.Should().NotBeNull();
             target.Should().BeOfType<TestObject>();
             target.Should().BeEquivalentTo(new TestObject
             {
                 Title = "Title",
-                Body = "Body",
+                Date = new DateTime(2000, 1, 1, 12, 00, 00, DateTimeKind.Utc),
                 NestedTestObject = new NestedTestObject
                 {
                     Title = "NestedTitle"
-                }
+                },
+                TestArray = new[] { "value1", "value2" }
             });
         }
 
@@ -72,9 +83,11 @@ namespace Plugin.FirebasePushNotifications.Tests.Internals
         {
             public string Title { get; set; }
 
-            public string Body { get; set; }
+            public DateTime Date { get; set; }
 
             public NestedTestObject NestedTestObject { get; set; }
+
+            public string[] TestArray { get; set; }
         }
 
         public class NestedTestObject
