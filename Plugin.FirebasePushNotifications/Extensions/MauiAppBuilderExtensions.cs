762dc78c541b64abe7fa79e04419d91dbd6100be
Thomas Galliker <thomas@bluewin.ch>
2023-10-06 19:22:24 +0200

    Initialize firebase in UseFirebasePushNotifications using configurable options

--- Plugin.FirebasePushNotifications/Extensions/MauiAppBuilderExtensions.cs
+++ Plugin.FirebasePushNotifications/Extensions/MauiAppBuilderExtensions.cs
@@ -1,9 +1,11 @@
 ﻿#if ANDROID || IOS
 using Plugin.FirebasePushNotifications.Platforms;
+using Microsoft.Extensions.Logging;
 #endif
 
 #if IOS
-using Firebase.CloudMessaging;
+using UIKit;
+using UserNotifications;
 #endif
 
 using Microsoft.Maui.LifecycleEvents;
@@ -23,21 +25,60 @@ namespace Plugin.FirebasePushNotifications
 #if IOS
                 events.AddiOS(iOS => iOS.FinishedLaunching((_, launchOptions) =>
                 {
-                    FirebasePushNotificationManager.Initialize(launchOptions, autoRegistration: false);
+                    var loggerFactory = MauiUIApplicationDelegate.Current.Services.GetService<ILoggerFactory>();
+                    var logger = loggerFactory.CreateLogger("AppDelegate");
+
+                    if (launchOptions != null)
+                    {
+                        var isPushNotification =
+                            launchOptions.TryGetValue(UIApplication.LaunchOptionsRemoteNotificationKey, out var pushNotificationPayload) &&
+                            pushNotificationPayload != null;
+                        var isLocalNotification =
+                            launchOptions.TryGetValue(UIApplication.LaunchOptionsLocalNotificationKey, out var localNotificationPayload) &&
+                            localNotificationPayload != null;
+
+                        logger.LogDebug(
+                            $"FinishedLaunching with " +
+                            $"launchOptions[isPushNotification={isPushNotification}, " +
+                            $"isLocalNotification={isLocalNotification}]");
+                    }
+                    else
+                    {
+                        logger.LogDebug($"FinishedLaunching");
+                    }
+                    
+                    // Instead of FirebasePushNotificationManager.Initialize
+                    Firebase.Core.App.Configure();
+                    Firebase.CloudMessaging.Messaging.SharedInstance.AutoInitEnabled = defaultOptions.AutoInitEnabled;
 
                     // In order to get OnTokenRefresh event called by firebase push notification plugin,
                     // we have to assign Messaging.SharedInstance.Delegate manually.
                     // https://github.com/CrossGeeks/FirebasePushNotificationPlugin/issues/303#issuecomment-730393259
-                    Messaging.SharedInstance.Delegate = CrossFirebasePushNotification.Current as IMessagingDelegate;
+                    Firebase.CloudMessaging.Messaging.SharedInstance.Delegate = CrossFirebasePushNotification.Current as Firebase.CloudMessaging.IMessagingDelegate;
+                    UNUserNotificationCenter.Current.Delegate = CrossFirebasePushNotification.Current as IUNUserNotificationCenterDelegate;
 
                     return false;
                 }));
 #elif ANDROID
+                events.AddAndroid(android => android.OnApplicationCreate(d =>
+                {
+                    FirebasePushNotificationManager.NotificationActivityType = defaultOptions.Android.NotificationActivityType;
+                    FirebasePushNotificationManager.DefaultNotificationChannelId = defaultOptions.Android.DefaultNotificationChannelId;
+
+                    // TODO: Create StaticNotificationChannels
+                    //StaticNotificationChannels.UpdateChannels(Context);
+
+                    if (defaultOptions.AutoInitEnabled)
+                    {
+                        Firebase.FirebaseApp.InitializeApp(d.ApplicationContext);
+                        Firebase.Messaging.FirebaseMessaging.Instance.AutoInitEnabled = defaultOptions.AutoInitEnabled;
+                    }
+
+
+                }));
                 events.AddAndroid(android => android.OnCreate((activity, intent) =>
                 {
-                    // TODO: Setup Android specific services
-                    //FirebasePushNotificationManager.Initialize(activity, firebaseSettings)));
-
+                    Firebase.FirebaseApp.InitializeApp(activity);
                     IntentHandler.CheckAndProcessIntent(activity, activity.Intent);
                 }));
                 events.AddAndroid(android => android.OnNewIntent((activity, intent) =>
@@ -47,7 +88,7 @@ namespace Plugin.FirebasePushNotifications
 #endif
             });
 
-            //CrossFirebasePushNotification.Current...
+            builder.Services.AddSingleton(_ => CrossFirebasePushNotification.Current);
             return builder;
         }
     }
