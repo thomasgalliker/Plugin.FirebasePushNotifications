34cfcb0ac874d3d3e4c197ec8634298c42643c00
Thomas Galliker <thomas@bluewin.ch>
2023-10-06 19:22:41 +0200

    Cleanup

--- Plugin.FirebasePushNotifications/Platforms/Android/IntentHandler.cs
+++ Plugin.FirebasePushNotifications/Platforms/Android/IntentHandler.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Android.App;
+﻿using Android.App;
 using Android.Content;
 using Android.Util;
 
