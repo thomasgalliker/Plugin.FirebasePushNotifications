0564296f3f1955e2f22b40cf520c51544f4d988c
Thomas Galliker <thomas@bluewin.ch>
2023-10-06 19:25:03 +0200

    Add NotificationData as payload for push notifications

---
+++ Plugin.FirebasePushNotifications/Model/NotificationData.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using Plugin.FirebasePushNotifications.Internals;
+
+namespace Plugin.FirebasePushNotifications.Model
+{
+    public class NotificationData
+    {
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("body")]
+        public string Body { get; set; }
+
+        public string ToString()
+        {
+            var dict = DictionaryJsonConverter.Flatten(this);
+            return string.Join($",{Environment.NewLine}", dict.Select(d => $"{{{d.Key}, {d.Value ?? "null"}}}"));
+        }
+    }
+}
