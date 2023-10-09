ff57e1609676011d56579a5db6629ef85cc13fa3
Thomas Galliker <thomas@bluewin.ch>
2023-10-09 18:36:29 +0200

    Add comment

--- Plugin.FirebasePushNotifications/FirebasePushNotificationOptions.cs
+++ Plugin.FirebasePushNotifications/FirebasePushNotificationOptions.cs
@@ -19,7 +19,18 @@ namespace Plugin.FirebasePushNotifications
     {
         public virtual bool AutoInitEnabled { get; set; }
 
-        public virtual IQueueFactory QueueFactory { get; set; } = new NullQueueFactory();
+        /// <summary>
+        /// The factory used to create new queues to intercept push notification events.
+        /// Default: <c>null</c> (Queues are disabled).
+        /// </summary>
+        /// <remarks>
+        /// Use one of the predefined queues or create your own implementation of <see cref="IQueueFactory"/>.
+        ///<list type="bullet">
+        ///<item><see cref="InMemoryQueueFactory"/></item>
+        ///<item><see cref="PersistentQueueFactory"/></item>
+        ///</list>
+        /// </remarks>
+        public virtual IQueueFactory QueueFactory { get; set; }
 
 #if ANDROID
 
