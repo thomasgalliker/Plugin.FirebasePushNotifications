2d2dc38dbb1c6e82929f2d841c53a50e8e90942d
Thomas Galliker <thomas@bluewin.ch>
2023-10-19 11:01:29 +0200

    Add subscribe/unsubscribe buttons in sample app

--- Samples/MauiSampleApp/ViewModels/MainViewModel.cs
+++ Samples/MauiSampleApp/ViewModels/MainViewModel.cs
@@ -15,18 +15,14 @@ namespace MauiSampleApp.ViewModels
 
         private AsyncRelayCommand registerForPushNotificationsCommand;
         private AsyncRelayCommand unregisterForPushNotificationsCommand;
+        private AsyncRelayCommand subscribeEventsCommand;
+        private AsyncRelayCommand unsubscribeEventsCommand;
 
         public MainViewModel(
             ILogger<MainViewModel> logger,
-            IDialogService dialogService, 
+            IDialogService dialogService,
             IFirebasePushNotification firebasePushNotification)
         {
-            //var firebasePushNotification = CrossFirebasePushNotification.Current;
-
-            firebasePushNotification.TokenRefreshed += this.OnTokenRefresh;
-            firebasePushNotification.NotificationReceived += this.OnNotificationReceived;
-
-            firebasePushNotification.ClearAllNotifications();
             this.logger = logger;
             this.dialogService = dialogService;
             this.firebasePushNotification = firebasePushNotification;
@@ -46,7 +42,7 @@ namespace MauiSampleApp.ViewModels
                 await this.dialogService.ShowDialogAsync("Error", "Register for push notifications failed with exception", "OK");
             }
         }
-        
+
         public ICommand UnregisterForPushNotificationsCommand => this.unregisterForPushNotificationsCommand ??= new AsyncRelayCommand(this.UnregisterForPushNotificationsAsync);
 
         private async Task UnregisterForPushNotificationsAsync()
@@ -62,11 +58,47 @@ namespace MauiSampleApp.ViewModels
             }
         }
 
-        private async void OnNotificationReceived(object sender, FirebasePushNotificationDataEventArgs e)
-        {
-            this.logger.LogDebug($"OnNotificationReceived: {e.Data.ToDebugString()}");
 
-            await this.dialogService.ShowDialogAsync("FirebasePushNotification", "OnNotificationReceived", "OK");
+        public ICommand SubscribeEventsCommand => this.subscribeEventsCommand ??= new AsyncRelayCommand(this.SubscribeEvents);
+
+        private async Task SubscribeEvents()
+        {
+            try
+            {
+                this.firebasePushNotification.TokenRefreshed += this.OnTokenRefresh;
+                this.firebasePushNotification.NotificationReceived += this.OnNotificationReceived;
+                this.firebasePushNotification.NotificationOpened += this.OnNotificationOpened;
+                this.firebasePushNotification.NotificationAction += this.OnNotificationAction;
+                this.firebasePushNotification.NotificationDeleted += this.OnNotificationDeleted;
+                this.firebasePushNotification.NotificationError += this.OnNotificationError;
+
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError(ex, "SubscribeEvents failed with exception");
+                await this.dialogService.ShowDialogAsync("Error", "SubscribeEvents failed with exception", "OK");
+            }
+        }
+
+        public ICommand UnsubscribeEventsCommand => this.unsubscribeEventsCommand ??= new AsyncRelayCommand(this.UnsubscribeEvents);
+
+        private async Task UnsubscribeEvents()
+        {
+            try
+            {
+                this.firebasePushNotification.TokenRefreshed -= this.OnTokenRefresh;
+                this.firebasePushNotification.NotificationReceived -= this.OnNotificationReceived;
+                this.firebasePushNotification.NotificationOpened -= this.OnNotificationOpened;
+                this.firebasePushNotification.NotificationAction -= this.OnNotificationAction;
+                this.firebasePushNotification.NotificationDeleted -= this.OnNotificationDeleted;
+                this.firebasePushNotification.NotificationError -= this.OnNotificationError;
+
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError(ex, "UnsubscribeEvents failed with exception");
+                await this.dialogService.ShowDialogAsync("Error", "UnsubscribeEvents failed with exception", "OK");
+            }
         }
 
         private async void OnTokenRefresh(object sender, FirebasePushNotificationTokenEventArgs e)
@@ -75,5 +107,36 @@ namespace MauiSampleApp.ViewModels
 
             await this.dialogService.ShowDialogAsync("FirebasePushNotification", "OnTokenRefresh", "OK");
         }
+
+        private async void OnNotificationReceived(object sender, FirebasePushNotificationDataEventArgs e)
+        {
+            this.logger.LogDebug($"OnNotificationReceived: {e.Data.ToDebugString()}");
+            await this.dialogService.ShowDialogAsync("FirebasePushNotification", "OnNotificationReceived", "OK");
+        }
+
+        private async void OnNotificationOpened(object sender, FirebasePushNotificationResponseEventArgs e)
+        {
+            this.logger.LogDebug($"OnNotificationOpened: {e.Data.ToDebugString()}");
+            await this.dialogService.ShowDialogAsync("FirebasePushNotification", "OnNotificationOpened", "OK");
+        }
+        
+        private async void OnNotificationAction(object sender, FirebasePushNotificationResponseEventArgs e)
+        {
+            this.logger.LogDebug($"OnNotificationAction: {e.Data.ToDebugString()}");
+            await this.dialogService.ShowDialogAsync("FirebasePushNotification", "OnNotificationAction", "OK");
+        }
+        
+        private async void OnNotificationDeleted(object sender, FirebasePushNotificationDataEventArgs e)
+        {
+            this.logger.LogDebug($"OnNotificationDeleted: {e.Data.ToDebugString()}");
+            await this.dialogService.ShowDialogAsync("FirebasePushNotification", "OnNotificationDeleted", "OK");
+        }
+
+        private async void OnNotificationError(object sender, FirebasePushNotificationErrorEventArgs e)
+        {
+            this.logger.LogDebug($"OnNotificationError: {e.Message}");
+            await this.dialogService.ShowDialogAsync("FirebasePushNotification", $"OnNotificationError: {e.Message}", "OK");
+        }
+
     }
 }
