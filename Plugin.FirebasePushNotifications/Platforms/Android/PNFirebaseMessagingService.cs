e8ad9fa6e716e9da720420f3077f51232e692119
Thomas Galliker <thomas@bluewin.ch>
2023-10-24 20:37:06 +0200

    Rename variable

--- Plugin.FirebasePushNotifications/Platforms/Android/PNFirebaseMessagingService.cs
+++ Plugin.FirebasePushNotifications/Platforms/Android/PNFirebaseMessagingService.cs
@@ -10,78 +10,78 @@ namespace Plugin.FirebasePushNotifications.Platforms
     {
         public override void OnMessageReceived(RemoteMessage message)
         {
-            var parameters = new Dictionary<string, object>();
+            var data = new Dictionary<string, object>();
             var notification = message.GetNotification();
             if (notification != null)
             {
                 if (!string.IsNullOrEmpty(notification.Body))
                 {
-                    parameters.Add("body", notification.Body);
+                    data.Add("body", notification.Body);
                 }
 
                 if (!string.IsNullOrEmpty(notification.BodyLocalizationKey))
                 {
-                    parameters.Add("body_loc_key", notification.BodyLocalizationKey);
+                    data.Add("body_loc_key", notification.BodyLocalizationKey);
                 }
 
                 var bodyLocArgs = notification.GetBodyLocalizationArgs();
                 if (bodyLocArgs != null && bodyLocArgs.Any())
                 {
-                    parameters.Add("body_loc_args", bodyLocArgs);
+                    data.Add("body_loc_args", bodyLocArgs);
                 }
 
                 if (!string.IsNullOrEmpty(notification.Title))
                 {
-                    parameters.Add("title", notification.Title);
+                    data.Add("title", notification.Title);
                 }
 
                 if (!string.IsNullOrEmpty(notification.TitleLocalizationKey))
                 {
-                    parameters.Add("title_loc_key", notification.TitleLocalizationKey);
+                    data.Add("title_loc_key", notification.TitleLocalizationKey);
                 }
 
                 var titleLocArgs = notification.GetTitleLocalizationArgs();
                 if (titleLocArgs != null && titleLocArgs.Any())
                 {
-                    parameters.Add("title_loc_args", titleLocArgs);
+                    data.Add("title_loc_args", titleLocArgs);
                 }
 
                 if (!string.IsNullOrEmpty(notification.Tag))
                 {
-                    parameters.Add("tag", notification.Tag);
+                    data.Add("tag", notification.Tag);
                 }
 
                 if (!string.IsNullOrEmpty(notification.Sound))
                 {
-                    parameters.Add("sound", notification.Sound);
+                    data.Add("sound", notification.Sound);
                 }
 
                 if (!string.IsNullOrEmpty(notification.Icon))
                 {
-                    parameters.Add("icon", notification.Icon);
+                    data.Add("icon", notification.Icon);
                 }
 
                 if (notification.Link != null)
                 {
-                    parameters.Add("link_path", notification.Link.Path);
+                    data.Add("link_path", notification.Link.Path);
                 }
 
                 if (!string.IsNullOrEmpty(notification.ClickAction))
                 {
-                    parameters.Add("click_action", notification.ClickAction);
+                    data.Add("click_action", notification.ClickAction);
                 }
 
                 if (!string.IsNullOrEmpty(notification.Color))
                 {
-                    parameters.Add("color", notification.Color);
+                    data.Add("color", notification.Color);
                 }
             }
 
             foreach (var d in message.Data)
             {
-                if (!parameters.ContainsKey(d.Key))
+                if (!data.ContainsKey(d.Key))
                 {
-                    parameters.Add(d.Key, d.Value);
+                    data.Add(d.Key, d.Value);
                 }
             }
 
@@ -89,22 +89,22 @@ namespace Plugin.FirebasePushNotifications.Platforms
             var localizationKeys = new string[] { "title_loc_args", "body_loc_args" };
             foreach (var locKey in localizationKeys)
             {
-                if (parameters.ContainsKey(locKey) && parameters[locKey] is string parameterValue)
+                if (data.ContainsKey(locKey) && data[locKey] is string parameterValue)
                 {
                     if (parameterValue.StartsWith("[") && parameterValue.EndsWith("]") && parameterValue.Length > 2)
                     {
 
                         var arrayValues = parameterValue[1..^1];
-                        parameters[locKey] = arrayValues.Split(',').Select(t => t.Trim()).ToArray();
+                        data[locKey] = arrayValues.Split(',').Select(t => t.Trim()).ToArray();
                     }
                     else
                     {
-                        parameters[locKey] = Array.Empty<string>();
+                        data[locKey] = Array.Empty<string>();
                     }
                 }
             }
 
-            CrossFirebasePushNotification.Current.HandleNotificationReceived(parameters);
+            CrossFirebasePushNotification.Current.HandleNotificationReceived(data);
         }
 
         public override void OnNewToken(string refreshedToken)
