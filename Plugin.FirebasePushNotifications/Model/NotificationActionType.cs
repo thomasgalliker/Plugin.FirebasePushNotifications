680367b8ac97ca8237f06b03662fab597d1667c5
Thomas Galliker <>
2024-07-31 08:06:22 +0200

    Add explicit enum values

--- Plugin.FirebasePushNotifications/Model/NotificationActionType.cs
+++ Plugin.FirebasePushNotifications/Model/NotificationActionType.cs
@@ -3,18 +3,18 @@
     public enum NotificationActionType
     {
         /// <summary>
-        /// When an action with this notification action type is tapped, 
-        /// the notification will be handled on background won't bring the application to foreground. 
+        /// When an action with this notification action type is tapped,
+        /// the notification will be handled on background won't bring the application to foreground.
         /// The action will take place without launching the application.
         /// </summary>
-        Default,
+        Default = 0,
 
         /// <summary>
         /// When an action with this notification action type is tapped,
-        /// it will bring the application to foreground and process 
+        /// it will bring the application to foreground and process
         /// the notification once application is launched successfully.
         /// </summary>
-        Foreground,
+        Foreground = 1,
 
         /// <summary>
         /// If set, the user needs to insert the unlock code to launch the action in background.
@@ -22,7 +22,7 @@
         /// <remarks>
         /// Only applies to iOS (UNNotificationActionOptions.AuthenticationRequired).
         /// </remarks>
-        AuthenticationRequired,
+        AuthenticationRequired = 2,
 
         /// <summary>
         /// The notification action type that indicates a destructive action.
@@ -30,7 +30,7 @@
         /// <remarks>
         /// Only applies to iOS (UNNotificationActionOptions.Destructive).
         /// </remarks>
-        Destructive
+        Destructive = 3
 
         //TODO: Android "accept" / "decline" --> decline should be Foreground AND Destructive
     }
