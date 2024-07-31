e7d6f6ab7685ff9d0436b2a018fa5b275b0c91ff
Thomas Galliker <>
2024-07-31 08:06:35 +0200

    Extend unit tests

--- Tests/Plugin.FirebasePushNotifications.Tests/Model/NotificationCategoryTests.cs
+++ Tests/Plugin.FirebasePushNotifications.Tests/Model/NotificationCategoryTests.cs
@@ -9,45 +9,70 @@ namespace Plugin.FirebasePushNotifications.Tests.Model
         public void ShouldCreateNotificationCategory()
         {
             // Act
-            const string categoryId = "category1";
             var notificationActions = new []
             {
                 new NotificationAction("action1", "title1", NotificationActionType.Foreground),
                 new NotificationAction("action2", "title2", NotificationActionType.Destructive),
             };
-            var notificationCategory = new NotificationCategory(categoryId, notificationActions);
-
-            // TODO: Test serialization/deserialization
-            var test = JsonConvert.SerializeObject(notificationCategory);
+            var notificationCategory = new NotificationCategory("category1", notificationActions);
 
             // Assert
-            notificationCategory.CategoryId.Should().Be(categoryId);
-            notificationCategory.Actions.Should().HaveCount(notificationActions.Length);
+            notificationCategory.CategoryId.Should().Be("category1");
+            notificationCategory.Actions.Should().HaveCount(2);
         }
 
         [Fact]
-        public void ShouldCreateNotificationCategory_FromJson()
+        public void ShouldSerializeNotificationCategory()
         {
+            // Arrange
+            var notificationActions = new []
+            {
+                new NotificationAction("action1", "title1", NotificationActionType.Foreground),
+                new NotificationAction("action2", "title2", NotificationActionType.Destructive),
+            };
+            var notificationCategory = new NotificationCategory("category1", notificationActions);
+
             // Act
-            var notificationCategoryJson = "" +
+            var notificationCategoryJson = JsonConvert.SerializeObject(notificationCategory);
+
+            // Assert
+            notificationCategoryJson.Should().Be(
+                "{\"categoryId\":\"category1\"," +
+                "\"actions\":[{\"id\":\"action1\"," +
+                "\"title\":\"title1\",\"type\":1," +
+                "\"icon\":null}," +
+                "{\"id\":\"action2\"," +
+                "\"title\":\"title2\"," +
+                "\"type\":3," +
+                "\"icon\":null}]," +
+                "\"type\":0}");
+        }
+
+        [Fact]
+        public void ShouldDeserializeNotificationCategory()
+        {
+            // Arrange
+            const string notificationCategoryJson =
                 "{\"categoryId\":\"category1\"," +
                 " \"actions\":[" +
-                "   {\"Id\":\"action1\",\"Title\":\"title1\",\"Type\":0,\"Icon\":null}," +
-                "   {\"Id\":\"action2\",\"Title\":\"title2\",\"Type\":0,\"Icon\":null}" +
+                "   {\"Id\":\"action1\",\"Title\":\"title1\",\"Type\":1,\"Icon\":null}," +
+                "   {\"Id\":\"action2\",\"Title\":\"title2\",\"Type\":3,\"Icon\":null}" +
                 "  ]," +
-                "\"type\":0}";
+                "\"type\":2}";
 
+            // Act
             var notificationCategory = JsonConvert.DeserializeObject<NotificationCategory>(notificationCategoryJson);
 
             // Assert
             notificationCategory.CategoryId.Should().Be("category1");
             notificationCategory.Actions.Should().HaveCount(2);
+            notificationCategory.Type.Should().Be(NotificationCategoryType.Dismiss);
 
             var action1 = notificationCategory.Actions.ElementAt(0);
-            action1.Should().BeEquivalentTo(new NotificationAction("action1", "title1", NotificationActionType.Default));
+            action1.Should().BeEquivalentTo(new NotificationAction("action1", "title1", NotificationActionType.Foreground));
 
             var action2 = notificationCategory.Actions.ElementAt(1);
-            action2.Should().BeEquivalentTo(new NotificationAction("action2", "title2", NotificationActionType.Default));
+            action2.Should().BeEquivalentTo(new NotificationAction("action2", "title2", NotificationActionType.Destructive));
         }
     }
 }
