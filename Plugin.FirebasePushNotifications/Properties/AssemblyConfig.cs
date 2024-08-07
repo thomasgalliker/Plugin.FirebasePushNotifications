1fa39a2e411c00f09c425fbbaf5d9b965df7c2e0
Thomas Galliker <>
2024-08-07 16:37:22 +0200

    Add Preserve attribute for IFirebasePushNotification interface (all members)

--- Plugin.FirebasePushNotifications/Properties/AssemblyConfig.cs
+++ Plugin.FirebasePushNotifications/Properties/AssemblyConfig.cs
@@ -1,5 +1,8 @@
 ﻿using System.Runtime.CompilerServices;
+using Plugin.FirebasePushNotifications;
 
 [assembly: InternalsVisibleTo("Plugin.FirebasePushNotifications.Tests")]
 [assembly: InternalsVisibleTo("MauiSampleApp")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+
+[assembly: Preserve(typeof(IFirebasePushNotification), AllMembers = true)]
