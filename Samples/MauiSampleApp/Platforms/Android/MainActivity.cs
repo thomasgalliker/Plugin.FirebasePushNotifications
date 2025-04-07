aec5cd3c82a44dcc6845e0446db758e6dd045f1f
Thomas Galliker <>
2025-04-07 10:18:12 +0200

    Use LaunchMode SingleTask (instead of SingleInstance) in Android sample app

--- Samples/MauiSampleApp/Platforms/Android/MainActivity.cs
+++ Samples/MauiSampleApp/Platforms/Android/MainActivity.cs
@@ -1,13 +1,12 @@
 ﻿using Android.App;
 using Android.Content.PM;
-using Android.OS;
 
 namespace MauiSampleApp
 {
     [Activity(
         Theme = "@style/Maui.SplashTheme",
         MainLauncher = true,
-        LaunchMode = LaunchMode.SingleInstance,
+        LaunchMode = LaunchMode.SingleTask,
         ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     [IntentFilter(new[] { "medication_intake" }, Categories = new[] { "android.intent.category.DEFAULT" })]
     public class MainActivity : MauiAppCompatActivity
