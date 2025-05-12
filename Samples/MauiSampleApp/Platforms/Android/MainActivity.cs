aa8c84ca62105b8f41e14f7da0c3a9e726d5add3
Thomas Galliker <>
2025-05-12 08:26:22 +0200

    Cleanup

--- Samples/MauiSampleApp/Platforms/Android/MainActivity.cs
+++ Samples/MauiSampleApp/Platforms/Android/MainActivity.cs
@@ -7,7 +7,12 @@ namespace MauiSampleApp
         Theme = "@style/Maui.SplashTheme",
         MainLauncher = true,
         LaunchMode = LaunchMode.SingleTask,
-        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+        ConfigurationChanges = ConfigChanges.ScreenSize |
+                               ConfigChanges.Orientation |
+                               ConfigChanges.UiMode |
+                               ConfigChanges.ScreenLayout |
+                               ConfigChanges.SmallestScreenSize |
+                               ConfigChanges.Density)]
     [IntentFilter(new[] { "medication_intake" }, Categories = new[] { "android.intent.category.DEFAULT" })]
     public class MainActivity : MauiAppCompatActivity
     {
